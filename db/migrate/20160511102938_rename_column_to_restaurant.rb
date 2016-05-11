10ce49d8887d6475a6cd4d5d3e1de6c824dc42f9
Guillaume Lavoisier <lavoisierguillaume@gmail.com>
2016-05-11 14:05:21 +0200

    ok

--- db/migrate/20160511102938_rename_column_to_restaurant.rb
+++ db/migrate/20160511102938_rename_column_to_restaurant.rb
@@ -1,4 +1,6 @@
 class RenameColumnToRestaurant < ActiveRecord::Migration
   def change
+    rename_column :restaurants, :phone_number
+    add_column :restaurants, :phone_number, :string, default: 0
   end
 end
